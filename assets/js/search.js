46b9fbbc7e7763490fea56a6f0d0fffe987e1438
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2018-01-27 15:58:18 +0100

    Search (#16)
    * Create search site
    
    * Use my new search.js code.
    
    * Use the right index
    
    * Use search.js
    
    * Update index.html

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,9 +1,25 @@
-function fmt_show_results (data)
+function Result (title, url, fragment, score)
 {
-	var items = data['items'];
+	this.title = title;
+	this.url = url;
+	this.fragment = fragment;
+	this.score = score;
+}
+
+function sjs_sort_score (a, b)
+{
+	if (a.score > b.score)
+		return -1;
+	if (a.score < b.score)
+		return 1;
+	return 0;
+}
+
+function sjs_show_results (results)
+{
 	var section = document.querySelector('section');
 	
-	if (data.total_count === 0)
+	if (results.length === 0)
 	{
 		var err = document.createElement ('h3');
 		err.textContent = "We couldn't find any page.";
@@ -11,43 +27,68 @@ function fmt_show_results (data)
 		return;
 	}
 	
-	for (var i = 0; i < items.length; i++)
+	for (var i = 0; i < results.length; i++)
 	{
 		var res = document.createElement ('article');
-		var res_name = document.createElement ('h2');
+
 		var res_link = document.createElement ('a');
-		
-		res_link.textContent = items[i].name.replace ('.md', '');
-		res_link.href = "http://fmt.one/wiki/" + res_link.textContent;
-		
+		res_link.textContent = results[i]['title'];
+		res_link.href = results[i]['url'];
+
+		var res_name = document.createElement ('h2');
 		res_name.appendChild (res_link);
 		res.appendChild (res_name);
-		
-		if (items[i].text_matches.length >= 1)
-		{
-			var res_desc = document.createElement ('p');
-			res_desc.textContent = items[i].text_matches[0].fragment;
-			res.appendChild (res_desc);
-		}
+
+		var res_desc = document.createElement ('p');
+		res_desc.textContent = results[i].fragment +
+		                       String.fromCharCode (0x2026);
+		res.appendChild (res_desc);
 		
 		section.appendChild (res);
 	}
 }
 
-function fmt_search (q)
+function sjs_rank (index, q)
+{
+	var results = [];
+	var body = "";
+	var score = 0;
+	
+	for (var i = 0; i < index.length; i++)
+	{
+		if (index[i].body === undefined)
+			continue;
+		
+		body = index[i].body.toLowerCase ();
+		
+		if (!body.includes (q))
+			continue;
+		
+		var myre = new RegExp ("(" + q + ")", "g");
+		score = body.match (myre);
+		
+		results.push (new Result (index[i].title,
+		                          index[i].url,
+		                          index[i].body.substring (0, 140),
+		                          score.length));
+	}
+	
+	results.sort (sjs_sort_score);
+	sjs_show_results (results);
+}
+
+function sjs_search (q)
 {
 	document.querySelector("input[name='q']").value = decodeURI (q);
 	
-	var url = "https://api.github.com/search/code?q=" + q + "+in:file+path:/wiki/+repo:marc-q/fmtone";
 	var req = new XMLHttpRequest ();
-	req.open ('GET', url);
+	req.open ('GET', "http://fmt.one/search.json");
 	req.responseType = 'json';
-	req.setRequestHeader ('Accept', 'application/vnd.github.v3.text-match+json');
 	req.send ();
 	
 	req.onload = function ()
 	{
 		var items = req.response;
-		fmt_show_results (items);
+		sjs_rank (items, q.toLowerCase ());
 	}
 }
